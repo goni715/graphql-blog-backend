7e0b80f32cb608274a43c15573c34d309a15aca7
goni715 <halesgoni715149123@gmail.com>
2026-06-16 21:39:47 +0600

    graphql schema defined

---
+++ src/schema.ts
@@ -0,0 +1,31 @@
+export const typeDefs = `#graphql
+  type Post{
+    id: ID!
+    title: String!
+    content: String!
+    author: User
+    published: Boolean
+    createdAt: String
+  }
+
+  type User{
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String
+    posts: [Post]
+    profile: Profile
+  }
+
+  type Profile {
+    id: ID!
+    bio: String
+    createdAt: String
+    user: User!
+  }
+
+  type Query {
+    me: User
+    posts: [Post]
+  }
+`;
